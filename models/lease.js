a4218287c511bc6cf3993ca35dfa31e83a4c1074
ethesx <ethesx@gmail.com>
2013-12-04 19:26:00 -0500

    Update lease.js

--- models/lease.js
+++ models/lease.js
@@ -4,13 +4,22 @@ var mongoose = require('mongoose')
 /* Our schema which we'll store whatever they're looking to send back to the requestor.*/
 
 var LeaseSchema = new Schema({
-  msrp: String,
-  baseCapCost: String,
-  baseCapCost: String,
-  baseCapCost: String,
-  baseCapCost: String,
-  baseCapCost: String,
-  baseCapCost: String,
+  msrp: Number,
+  baseCapCost: Number,
+  costsAdded: Number,
+  capCostReduction: Number,
+  adjustedCapCost: Number,
+  residualValue: Number,
+  moneyFactor: Number,
+  term: Number,
+  termUnit: String,
+  salesTax: Number,
+  payment: Number,
+  paymentUnit: String,
+  financeCharge: Number,
+  salesTaxTot: Number,
+  totPayments: Number,
+  totLeaseCost: Number
 });
 
 mongoose.model('Lease', LeaseSchema);
